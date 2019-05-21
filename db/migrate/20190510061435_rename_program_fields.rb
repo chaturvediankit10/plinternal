362c40780bce1888a63c10f7e3188c3b5ea59d6e
Shiv <shiv@gmail.com>
2019-05-21 18:05:49 +0530

    commented migration code

--- db/migrate/20190510061435_rename_program_fields.rb
+++ db/migrate/20190510061435_rename_program_fields.rb
@@ -1,6 +1,6 @@
 class RenameProgramFields < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :programs, :du, :fannie_mae_du
-  	rename_column :programs, :lp, :freddie_mac_lp
+  	# rename_column :programs, :du, :fannie_mae_du
+  	# rename_column :programs, :lp, :freddie_mac_lp
   end
 end
