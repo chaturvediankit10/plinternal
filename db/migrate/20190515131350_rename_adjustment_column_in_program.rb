e827eb9f4dbf40a932a5a0b0fc535d421eff1561
Shiv <shiv@gmail.com>
2019-05-21 18:18:25 +0530

    uncomment migration

--- db/migrate/20190515131350_rename_adjustment_column_in_program.rb
+++ db/migrate/20190515131350_rename_adjustment_column_in_program.rb
@@ -3,6 +3,6 @@ class RenameAdjustmentColumnInProgram < ActiveRecord::Migration[5.1]
   	rename_column :programs, :adjustments, :adjustment_ids
   	# rename_column :programs, :du, :fannie_mae_du
   	# rename_column :programs, :lp, :freddie_mac_lp
-  	# add_column :programs, :arm_caps, :string
+  	add_column :programs, :arm_caps, :string
   end
 end
