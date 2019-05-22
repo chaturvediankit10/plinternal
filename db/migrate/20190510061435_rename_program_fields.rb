5cd4d16563222b1de4bf9b6e9d4ef25bf0c29ed8
Shiv <shiv@gmail.com>
2019-05-22 15:40:32 +0530

    resolve migration issue

--- db/migrate/20190510061435_rename_program_fields.rb
+++
@@ -1,6 +0,0 @@
-class RenameProgramFields < ActiveRecord::Migration[5.2]
-  def change
-  	# rename_column :programs, :du, :fannie_mae_du
-  	# rename_column :programs, :lp, :freddie_mac_lp
-  end
-end
