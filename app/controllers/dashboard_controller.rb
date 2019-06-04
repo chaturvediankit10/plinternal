e6b41fdde63cfdc0dbf4c52cfa1a791aae9466a7
Kendrick <kendrick.d@gmail.com>
2019-06-04 14:00:41 +0530

    Added profiling method for production

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < SearchApi::DashboardController
+  before_action :profiling
 
   def home
     list_of_banks_and_programs_with_search_results    
@@ -6,5 +7,9 @@ class DashboardController < SearchApi::DashboardController
 
   def fetch_programs
     fetch_programs_by_bank
+  end
+
+  def profiling
+    Rack::MiniProfiler.authorize_request
   end
 end
